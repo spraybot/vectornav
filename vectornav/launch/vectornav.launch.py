e3a70904031532b9265ec7f3468e9220c72bfdfe
Shrijit Singh <shrijitsingh99@gmail.com>
2022-01-05 04:39:41 -0500

    Add playback mode to launch file

--- vectornav/launch/vectornav.launch.py
+++ vectornav/launch/vectornav.launch.py
@@ -4,17 +4,22 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import UnlessCondition
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
 
     this_dir = get_package_share_directory('vectornav')
+
+    playback_mode = DeclareLaunchArgument('playback', default_value='False')
     
-    # Vectornav
     start_vectornav_cmd = Node(
         package='vectornav', 
         executable='vectornav',
         output='screen',
-        parameters=[os.path.join(this_dir, 'config', 'vectornav.yaml')])
+        parameters=[os.path.join(this_dir, 'config', 'vectornav.yaml')],
+        condition=UnlessCondition(LaunchConfiguration("playback")))
     
     start_vectornav_sensor_msgs_cmd = Node(
         package='vectornav', 
@@ -25,7 +30,10 @@ def generate_launch_description():
     # Create the launch description and populate
     ld = LaunchDescription()
 
-    ld.add_action(start_vectornav_cmd)
+    ld.add_action(playback_mode)
+    
+    if playback_mode:
+        ld.add_action(start_vectornav_cmd)
     ld.add_action(start_vectornav_sensor_msgs_cmd)
 
     return ld
